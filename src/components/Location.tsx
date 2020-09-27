5a89cf18f44775d0c688cd130a9cb6f3fa39d265
Erik Stockmeier <erik.d.stock@gmail.com>
2020-09-27 12:03:58 -0500

    add typescript

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,4 +1,8 @@
-export const Location = ({ location }) => {
+interface Props {
+  location: Position;
+}
+
+export const Location: React.FC<Props> = ({ location }) => {
   return (
     <p>
       Your location is {location.coords.latitude}, {location.coords.longitude}
